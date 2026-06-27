5ce1c1cfbeb4c9dd01d8228a062198e6ab329b2d
Андрей Королько <andrey.korolko@mail.ru>
2026-06-27 17:38:43 +0300

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+    "fmt"
+    "os"
+    "strconv"
+
+    "github.com/joho/godotenv"
+)
+
+type Config struct {
+    DBUser     string
+    DBPassword string
+    DBName     string
+    DBHost     string
+    DBPort     int
+    DBSSLMode  string
+    AppPort    string
+    AppEnv     string
+}
+
+func Load() *Config {
+    _ = godotenv.Load()
+
+    return &Config{
+        DBUser:     getEnv("POSTGRES_USER", "postgres"),
+        DBPassword: getEnv("POSTGRES_PASSWORD", ""),
+        DBName:     getEnv("POSTGRES_DB", "subscriptions"),
+        DBHost:     getEnv("DB_HOST", "postgres"),
+        DBPort:     getEnvAsInt("DB_PORT", 5432),
+        DBSSLMode:  getEnv("DB_SSLMODE", "disable"),
+        AppPort:    getEnv("APP_PORT", "8080"),
+        AppEnv:     getEnv("APP_ENV", "development"),
+    }
+}
+
+func (c *Config) DSN() string {
+    return fmt.Sprintf(
+        "postgres://%s:%s@%s:%d/%s?sslmode=%s",
+        c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName, c.DBSSLMode,
+    )
+}
+
+func (c *Config) Addr() string {
+    return fmt.Sprintf(":%s", c.AppPort)
+}
+
+func getEnv(key, defaultVal string) string {
+    if val := os.Getenv(key); val != "" {
+        return val
+    }
+    return defaultVal
+}
+
+func getEnvAsInt(key string, defaultVal int) int {
+    if val := os.Getenv(key); val != "" {
+        if i, err := strconv.Atoi(val); err == nil {
+            return i
+        }
+    }
+    return defaultVal
+}
