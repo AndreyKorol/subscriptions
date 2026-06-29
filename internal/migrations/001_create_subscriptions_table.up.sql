70ad13a8f3ce0bd8a19a58527ddc7581a6a3c63b
Андрей Королько <andrey.korolko@mail.ru>
2026-06-29 08:40:02 +0300

    improve migration: add indexes

--- internal/migrations/001_create_subscriptions_table.up.sql
+++ internal/migrations/001_create_subscriptions_table.up.sql
@@ -7,4 +7,6 @@ CREATE TABLE IF NOT EXISTS subscriptions (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
-CREATE INDEX idx_subscriptions_user_start ON subscriptions (user_id, start_date);
+CREATE INDEX IF NOT EXISTS idx_subscriptions_user_start ON subscriptions (user_id, start_date);
+CREATE INDEX IF NOT EXISTS idx_subscriptions_service_name ON subscriptions (service_name);
+CREATE INDEX IF NOT EXISTS idx_subscriptions_start_date ON subscriptions (start_date);
